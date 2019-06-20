08f6608dd5b496fb9e8dcee6ed5b00ad0522ef21
phirmware <chibuzor.ojukwu@gmail.com>
2019-06-20 22:12:24 +0000

    start template change

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { EnquireUniportService } from '../enquire-uniport.service';
 
 @Component({
   selector: 'app-home',
@@ -10,10 +11,27 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 export class HomeComponent implements OnInit {
 
   helper = new JwtHelperService();
+  availableCourses;
+  fromStorage = localStorage.getItem('available-courses');
+  courses;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private service: EnquireUniportService) { }
 
   ngOnInit() {
+    if (this.helper.isTokenExpired(this.fromStorage)) {
+      console.log('Expired , go to the network');
+      this.service.getCoursesToken().subscribe(res => {
+        this.availableCourses = res['token'];
+        localStorage.setItem('available-courses', this.availableCourses);
+        var getToken = localStorage.getItem('available-courses');
+        this.courses = this.helper.decodeToken(getToken)['courses'];
+      });
+    } else {
+      console.log('Not expired , Decode token');
+      this.courses = this.helper.decodeToken(this.fromStorage)['courses'];
+    }
+
+    console.log(this.courses);
   }
 
   nav() {
