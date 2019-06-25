99c48e90e7ee7e85aade0e11ff355b4c412a8c9e
phirmware <chibuzor.ojukwu@gmail.com>
2019-06-25 09:34:34 +0000

    nav functions

--- src/app/faq/faq.component.ts
+++ src/app/faq/faq.component.ts
@@ -9,6 +9,18 @@ export class FAQComponent implements OnInit {
 
   constructor() { }
 
+  faq = [
+    {
+      question: 'What is Jefftutors?', answer: `Jefftutors is a platform that was created to make
+    learning easier for students along with other side benefits. There are available courses that are constantly
+    updated
+    for the benefit of students.`},
+    {
+      question: ` How do I subscribe for a course?`, answer: ` You can pay for a course using two options, either
+      your bank account or your ATM card`
+    }
+  ]
+
   ngOnInit() { }
 
   contactMe() {
