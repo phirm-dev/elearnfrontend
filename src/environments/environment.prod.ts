d8320e625b23648db914c7f0e1ca978e3986e737
phirmware <chibuzor.ojukwu@gmail.com>
2020-01-13 22:48:59 +0100

    changed backend url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  url : 'https://jefftutors-node.nerdeveloper.now.sh/api/',
+  // url : 'https://jefftutors-node.nerdeveloper.now.sh/api/',
+  url: 'https://studykrib.herokuapp.com/api/',
   baseVideoUrl: 'https://storage.googleapis.com/globally-cdn-jefftutors',
 };
