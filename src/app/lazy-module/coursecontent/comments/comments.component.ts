a74b4ef7da26b4eac7c5b66137b71b0f26eef541
phirmware <chibuzor.ojukwu@gmail.com>
2020-04-13 08:19:40 +0100

    cleaned up comment section

--- src/app/lazy-module/coursecontent/comments/comments.component.ts
+++ src/app/lazy-module/coursecontent/comments/comments.component.ts
@@ -14,17 +14,4 @@ export class CommentsComponent implements OnInit {
 
   ngOnInit() { }
 
-  // makeComment(comment, course) {
-  //   if (comment.value === '') {
-  //     swal('Fill In Comment');
-  //     return;
-  //   }
-  //   const sentence = comment.value;
-  //   comment.value = '';
-  //   const username = this.helper.decodeToken(this.token).username;
-  //   this.service.makeComment(username, sentence, course).subscribe(res => {
-  //     this.comments$.push(res);
-  //   });
-  // }
-
 }
