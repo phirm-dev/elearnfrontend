c14c8f0d82fc9d77913c5fb2cf468c61bc0873e9
phirmware <chibuzor.ojukwu@gmail.com>
2021-05-09 00:46:21 +0100

    feat: add material section to student dashboard

--- src/app/lazy-module/coursecontent/overview/overview.component.ts
+++ src/app/lazy-module/coursecontent/overview/overview.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class OverviewComponent implements OnInit {
 
   @Input() description: any;
+  @Input() materials: string;
 
   constructor() { }
 
